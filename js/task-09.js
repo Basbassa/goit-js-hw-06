0c75de6efa61b09df2308ab6a007e94e0d7e5cc7
Anna <basbassa@gmail.com>
2023-03-15 22:23:35 +0100

    task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const widget = document.querySelector(".widget");
+const colorSpan = widget.querySelector(".color");
+const changeColorButton = widget.querySelector(".change-color");
+const body = document.querySelector("body");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColorButton.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+
+  body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
